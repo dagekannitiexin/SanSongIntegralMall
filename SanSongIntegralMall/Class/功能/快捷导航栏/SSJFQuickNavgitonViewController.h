eac983eda9f2e86e8da4c009f8aa40742f9a64b1
YouDa LIn <dagekannitiexin@users.noreply.github.com>
2018-01-15 11:22:42 +0800

    1.15

---
+++ SanSongIntegralMall/Class/功能/快捷导航栏/SSJFQuickNavgitonViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSJFQuickNavgitonViewController.h
+//  SanSongIntegralMall
+//
+//  Created by apple on 2018/1/15.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import "XMBaseViewController.h"
+
+@interface SSJFQuickNavgitonViewController : XMBaseViewController
+
+@end
